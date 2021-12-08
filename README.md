68e8e4d42b0da83242dfc956d3e42fe7e992319b
Amir-zeb <amirzeb695@gmail.com>
2021-12-08 23:44:53 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProjectCampaway
+ 
---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+var user_dropdown = document.querySelector('.user_dropdown');
+var user_dropdown_list = document.querySelector('.user_dropdown_list');
+if (user_dropdown) {
+    user_dropdown.addEventListener('click', () => {
+        user_dropdown_list.classList.toggle("show");
+    })
+}
+
+var close_media_sec = document.querySelector('.close_media_sec');
+var open_media_btn = document.querySelector('.open_media_btn');
+var media_sec = document.querySelector('.media_sec');
+if (close_media_sec) {
+    close_media_sec.addEventListener('click', () => {
+        media_sec.classList.add("close");
+    })
+}
+if (open_media_btn) {
+    open_media_btn.addEventListener('click', () => {
+        media_sec.classList.remove("close");
+    })
+}
+
+console.log("there");
